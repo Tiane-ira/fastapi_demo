af7716a608eeeb0995ec14752ee525e50bb096da
13972762373 <574635222@qq.com>
2024-12-26 21:36:02 +0800

    update:日志输出时指定utf-8

--- log/__init__.py
+++ log/__init__.py
@@ -32,13 +32,13 @@ log_abs_name = f"{log_path}/{log_name}"
 rotation = CONF['log']['rotation']
 if rotation == "1":
     # 按天分割文件日志
-    time_rotating_file_handler = handlers.TimedRotatingFileHandler(filename=log_abs_name, when='D')
+    time_rotating_file_handler = handlers.TimedRotatingFileHandler(filename=log_abs_name, when='D', encoding='utf-8')
     time_rotating_file_handler.setLevel(level)
     time_rotating_file_handler.setFormatter(formatter)
     logger.addHandler(time_rotating_file_handler)
 else:
     # 完整日志文件
-    file_handler = logging.FileHandler(log_abs_name)
+    file_handler = logging.FileHandler(log_abs_name, encoding='utf-8')
     file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
@@ -46,6 +46,12 @@ else:
 # 输出日志到控制台
 console = CONF['log']['console']
 if console == "1":
+    import sys
+    import io
+
+    # 设置标准输出和标准错误的编码为UTF-8
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(level)
     stream_handler.setFormatter(formatter)
